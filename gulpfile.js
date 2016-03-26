15b608557587c37dd8dcd4cf626270c4b0ea23a8
LeonDEXZ <leon.zharikov@gmail.com>
2016-03-26 17:37:53 +0300

    gulp init

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,74 @@ var gulp			= require('gulp'),
 	sass			= require('gulp-sass'),
 	browserSync		= require('browser-sync').create(),
 	autoprefixer 	= require('gulp-autoprefixer');
+
+var baseDir = "app",
+	distDir = "build";
+
+gulp.task('styles', function() {
+	return gulp.src(baseDir + '/sass/*.sass')
+		.pipe(sass({
+			includePaths: require('node-bourbon').includePaths
+		}).on('error', sass.logError))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest(baseDir + '/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('libs-i', function() {
+	// font-awesome
+	gulp.src(baseDir + '/libs/font-awesome/css/font-awesome.min.css')
+		.pipe(gulp.dest(baseDir + '/css'));
+
+	gulp.src(baseDir + '/libs/font-awesome/fonts/*.*')
+		.pipe(gulp.dest(baseDir + '/fonts'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+			baseDir + '/libs/jquery/dist/jquery.min.js'
+		])
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(baseDir + '/js'));
+});
+
+gulp.task('browser-sync', ['styles', 'scripts'], function() {
+	browserSync.init({
+		server: {
+			baseDir: baseDir
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', function() {
+	gulp.watch(baseDir + '/sass/*.sass', ['styles']);
+	gulp.watch(baseDir + '/libs/**/*.js', ['scripts']);
+	gulp.watch(baseDir + '/js/*.js').on("change", browserSync.reload);
+	gulp.watch(baseDir + '/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('clean', function() {
+	del([
+		distDir + '/css/*.css',
+		distDir + '/index.html',
+		distDir + '/js/*.js'
+	]);
+});
+
+gulp.task('build', ['clean', 'styles', 'scripts'], function() {
+
+	gulp.src(baseDir + '/css/*.css')
+		.pipe(gulp.dest(distDir + '/css'));
+
+	gulp.src(baseDir + '/*.html')
+		.pipe(gulp.dest(distDir));
+
+	gulp.src(baseDir + '/js/*.js')
+		.pipe(gulp.dest(distDir + '/js'));	
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
